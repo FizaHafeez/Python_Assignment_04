6879c028629c4acc4c33e6e389511be8ee50ebb5
fizahafeez <fizahafeez405@gmail.com>
2025-04-17 03:51:59 +0500

    first commit

---
+++ Assignment_00_to_07/00_intro_python/06_square_number.py
@@ -0,0 +1,9 @@
+def main():
+    number = float(input(" \033[1;3m Type a number to see it'square: \033[0m "))
+
+    sqaure = number * number 
+
+    print(f"the sqaure of {number} is {sqaure}")
+
+if __name__ == "__main__":
+    main()
